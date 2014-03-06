57729a2dfde476e3757b3ac1a57d94585799b24b
Spartanace1024 <spartanace1024@gmail.com>
2014-03-06 06:19:23 -0800

    Set up the scene
    -Finished setting up the scene and adding waypoints.
    -Made it look cool

---
+++ Assets/Scripts/Triangle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Triangle : MonoBehaviour {
+    public GameObject circle;
+    public Transform point;
+    private NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(point.position);
+	}
+}
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour {
+    public string nameOfImage;
+    void OnDrawGizmos() {
+        Gizmos.DrawIcon(transform.position, nameOfImage, true);
+    }
+}
