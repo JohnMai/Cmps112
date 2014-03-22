fdd9f5d0c7b4af4d64306ab291b2377ef9856886
John Mai <jxmai@ucsc.edu>
2014-03-21 22:26:47 -0700

    sdfsdf
    sdfsdf

--- Assets/Scripts/CircleBehaviorTree.js
+++ Assets/Scripts/CircleBehaviorTree.js
@@ -1,19 +1,30 @@
 ﻿#pragma strict
 
+var stop : GameObject;
+var channelHeal : GameObject;
+var dash  : GameObject;
+var move  : GameObject;
+var circleHP  : GameObject;
+var withinDis  : GameObject;
+var dash2  : GameObject;
+var explode : GameObject;
+var pickRanPoint  : GameObject;
+var leaveBomb : GameObject;
+var teleport  : GameObject;
 function Start () {
 	//sequence for stop then channel heal
 	var stopChannel = new Sequence();
-	var stop = new Stop();
-	var channelHeal = new ChannelHeal();
-	stopChannel.children.Add(stop);
+	//var stop : GameObject;
+	//var channelHeal : GameObject;
+	stopChannel.children.Add(stop.GetComponent(Stop));
 	stopChannel.children.Add(stopChannel);
 
 	//sequence for dash then move to the cloest point 3
 	var dashMove = new Sequence();
-	var dash = new Dash();
-	var move = new MoveToClosestPointThree();
-	dashMove.children.Add(dash);
-	dashMove.children.Add(move);
+	//var dash  : GameObject;
+	//var move  : GameObject;
+	dashMove.children.Add(dash.GetComponent(Dash));
+	dashMove.children.Add(move.GetComponent(MoveToClosestPointThree));
 	
 	//defensive tree 
 	var defensive = new RandomSelector();
@@ -22,27 +33,27 @@ function Start () {
 	
 	//defense when low hp tree 
 	var DWhenLow = new Sequence();
-	var circleHP = new CircleHPCondition();
-	DWhenLow.children.Add(circleHP);
+	//var circleHP  : GameObject;
+	DWhenLow.children.Add(circleHP.GetComponent(CircleHPCondition));
 	DWhenLow.children.Add(defensive);
 	
 	//aggressive tree
 	var aggressive = new Sequence();
-	var withinDis = new CircleDistanceCondition();
-	var dash2 = new Dash(); 
-	var explode = new Explode();
-	aggressive.children.Add(withinDis);
-	aggressive.children.Add(dash2);
-	aggressive.children.Add(explode);
+	//var withinDis  : GameObject;
+	//var dash2  : GameObject;
+	//var explode : GameObject;
+	aggressive.children.Add(withinDis.GetComponent(CircleDistanceCondition));
+	aggressive.children.Add(dash2.GetComponent(Dash));
+	aggressive.children.Add(explode.GetComponent(Explode));
 	
 	//hail mary tree
 	var hiMary = new Sequence();
-	var pickRanPoint = new PickRNDPointThree();
-	var leaveBomb = new LeaveBomb();
-	var teleport = new Teleport();
-	hiMary.children.Add(pickRanPoint);
-	hiMary.children.Add(leaveBomb);
-	hiMary.children.Add(teleport);
+	//var pickRanPoint  : GameObject;
+	//var leaveBomb : GameObject;
+	//var teleport  : GameObject;
+	hiMary.children.Add(pickRanPoint.GetComponent(PickRNDPointThree));
+	hiMary.children.Add(leaveBomb.GetComponent(LeaveBomb));
+	hiMary.children.Add(teleport.GetComponent(Teleport));
 	
 	//the entire circle tree
 	var circleTree = new Selector();
